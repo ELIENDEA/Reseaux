3d290c90964fda62b60200e5b79e07f2581e317c
Maxime Deroissart <37555058+MaximeDeus@users.noreply.github.com>
2018-03-25 20:17:20 +0200

    Mise à jour du TP3

--- tp3-interrogation-serveur-DNS_sasu_deroissart/src/main/java/fil/rsx/ReponsesTP3.java
+++ tp3-interrogation-serveur-DNS_sasu_deroissart/src/main/java/fil/rsx/ReponsesTP3.java
@@ -9,7 +9,7 @@ import java.net.InetAddress;
  * 	- Créer une requête DNS
  *  - L'envoyer à un serveur DNS
  *  - Récupérer la trame
- *  - Afficher l'adresse IP¨
+ *  - Afficher les alias ainsi que les adresses IP de type v4 et v6¨
  *  
  * @auteur DEROISSART Maxime | SASU Daniel
  */
@@ -80,9 +80,9 @@ public class ReponsesTP3 {
 	/**
 	 * Q4/Q5
 	 * 
-	 * Affiche la ou les adresses IP d'un nom de domaine et retourne l'entier correspondant aux 4 octets de l'adresse
+	 * Affiche la ou les adresses IP IPv4, IPv6 et enfin les alias d'un nom de domaine et retourne l'entier correspondant aux 4 octets de la dernière adresse IPv4
 	 * @param name : La forme symbolique d'un nom de domaine (ex : www.lifl.fr)
-	 * @return : la ou les adresses IP correspondantes (long 64 bits pour valeurs > (2*31)-1)
+	 * @return : la dernière adresse IPv4 (long 64 bits pour valeurs > (2*31)-1)
 	 */
 	public static long extraitIP(String nomDeDomaine)
 	{
@@ -160,7 +160,7 @@ public class ReponsesTP3 {
 		    if ((i+1)%16 == 0)
 			System.out.println("");
 		}
-		System.out.println("");
+		System.out.println("\n");
 		
 		
 		/* Partie principale : affichage des adresses IP */
@@ -169,10 +169,22 @@ public class ReponsesTP3 {
 		for(int i = 0; i < dp.getLength(); i++) {
 			if (rec [i] == (byte) 0xC0 ) /* Si on tombe sur un octet c0, synonyme de début de réponse */
 			{
-				if (rec[i+3]== (byte) 0x01) /*Si cela correspond à une requête de type A (host address d'après wireshark) */
+				if (rec[i+3]== (byte) 0x05) /*Si cela correspond à une requête de type CNAME (un alias) */
 				{
-					System.out.println("Adresse IP trouvée ! \n");
-					System.out.println("IP n°" + cpt +" : " + (rec [i+12] & 0xFF)+ "." +  (rec [i+13]& 0xFF) + "." + (rec [i+14]& 0xFF) + "." + (rec [i+15] & 0xFF)); /* affiche l'adresse au format standard */
+					System.out.print(nomDeDomaine + " is an alias for ");
+					int j = i+13;
+					while (rec[j] != (byte) 0xc0) /* Tant que l'on n'a pas atteint l'octet c0, marqueur de fin du nom de l'alias */
+					{
+					System.out.print((char)((rec[j]) & 0xff)) ; /* affiche l'alias octet par octet */
+	                j += 1; /* Incrémentation de l'index */
+					}
+					System.out.println(nomDeDomaine.substring(3)); /*Pour la fin de l'alias, affiche le nom de domaine initial, sans les "www" */
+				}
+					
+				if (rec[i+3]== (byte) 0x01) /*Si cela correspond à une requête de type A (IPv4) */
+				{
+					System.out.println("Adresse IPv4 trouvée !");
+					System.out.println("IP n°" + cpt +" : " + (rec [i+12] & 0xFF)+ "." +  (rec [i+13]& 0xFF) + "." + (rec [i+14]& 0xFF) + "." + (rec [i+15] & 0xFF) + "\n"); /* affiche l'adresse au format standard */
 	                cpt += 1; /* Incrémentation du compteur */
 					res += (rec [i+4] & 0xFF) << 24 & 0xFFFFFFFFL; /* Création du résultat, via un décalage pour placer les octets dans l'ordre. 
 					                                                  L = format long int (64 bits), car val max en int : (2**31 -1), soit 0x7FFFFFFF et non 2*32 -1, 
@@ -180,17 +192,58 @@ public class ReponsesTP3 {
 					res += (rec [i+5] & 0xFF) << 16 ;
 					res += (rec [i+6] & 0xFF) << 8 ;
 					res += (rec [i+7] & 0xFF) << 0 ;
-					System.out.println("héxadécimal :" + (Integer.toHexString(rec [i+4] & 0xFF))+ " " +  (Integer.toHexString(rec [i+5] & 0xFF)) + " " + (Integer.toHexString(rec [i+6] & 0xFF)) + " " + (Integer.toHexString(rec [i+7] & 0xFF)));
-					System.out.println("décimal :" + res);
-					System.out.println("binaire :" + Long.toBinaryString(res));
 					
+					/* N'a pas de sens réel en terme de valeur, mais possibilité de décommenter si l'on souhaite voir la "valeur" de l'adresse IP sous différents formats */
+					/** 
+					 * System.out.println("héxadécimal :" + (Integer.toHexString(rec [i+12] & 0xFF))+ " " +  (Integer.toHexString(rec [i+13] & 0xFF)) + " " + (Integer.toHexString(rec [i+14] & 0xFF)) + " " + (Integer.toHexString(rec [i+15] & 0xFF)));
+					 * System.out.println("décimal :" + res);
+					 * System.out.println("binaire :" + Long.toBinaryString(res));
+					 */
 				}
 			}
-			
-
-	    }
+		}
+		cpt = 1; /* Sert à numéroter les adresses IP */
+		
+		/** On reparcourt le datagramme reçu. Pas optimal, mais permet de séparer à l'affichage les adresses IP de type v4 et v6.
+		  * On aurait évidemment pu effectuer qu'un seul parcours, en stockant dans deux tableaux distincts selon la version de l'IP.
+		  */
+		
+		for(int i = 0; i < dp.getLength(); i++) {
+			if (rec[i+3]== (byte) 0x1c) /*Si cela correspond à une requête de type AAAA (IPv6) */
+			{
+				System.out.println("Adresse IPv6 trouvée !");
+                                System.out.print("IP n°" + cpt +" : ");
+				boolean valeurNulle = false; /* Permet de gérer correctement la syntaxe particulière de l'IPv6 en cas de suite de paire d'octets nuls */
+				int valeurPaireOctets; /* Stockage de la valeur de deux octets consécutifs */ 
+				for (int j = i+12 ; j < i+27 ; j+=2) /* On parcourt par paire les octets de l'IPv6 */
+				{
+					valeurPaireOctets = (((rec [j]) << 8) | rec [j+1]) & 0xFFFF;
+					if (valeurPaireOctets != (byte) 0x0000) /* Si valeur non nulle */
+					{
+						if (valeurNulle) /*Si fin de série de paire d'octets nuls */
+						{
+							System.out.print (":"); /*On met les ':' avant */
+							System.out.print(Integer.toHexString(valeurPaireOctets)) ; /* affiche l'alias octet par octet */
+						}
+						else
+						{
+							System.out.print(Integer.toHexString(valeurPaireOctets)) ; /* affiche l'alias octet par octet */
+							System.out.print (":"); /*On met les ':' après */
+						}
+						valeurNulle = false; /* l'octet inséré était forcément non nul */
+					}      
+					else
+					{
+						valeurNulle = true; /* Début d'une suite de paire d'octets nuls */
+					}
+                
+			    }
+				System.out.println ("\n");
+			cpt += 1; /* Incrémentation du compteur */
+		    }
+		}
 		ds.close();
-		return res; /* Dernière IP obtenue */
+		return res; /* Dernière IPv4 obtenue */
 	}
 
 	public static void main (String[] args)
@@ -202,4 +255,3 @@ public class ReponsesTP3 {
 	
 	    
 }
-
